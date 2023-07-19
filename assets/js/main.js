53cf9e35d8d20fc16b3ac21b08fe91beb4d2cac1
Roqi SB <roqisb9@gmail.com>
2023-07-19 20:02:42 +0700

    add js date

--- assets/js/main.js
+++ assets/js/main.js
@@ -301,3 +301,16 @@ roqi.addEventListener("click",function picture(){
 calvin.addEventListener("click",function picture(){
   alert("Ini adalah gambar Calvin sebagai Entrepreneur");
 });
+
+function updateDateTime() {
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var now = new Date();
+  var currentDay = days[now.getDay()];
+  var dateTime = now.toLocaleString();
+  var dateTimeText = currentDay + ", " + "<br>" + dateTime;
+
+  document.getElementById("currentDateTime").innerHTML = dateTimeText;
+
+}
+
+setInterval(updateDateTime, 1000);
